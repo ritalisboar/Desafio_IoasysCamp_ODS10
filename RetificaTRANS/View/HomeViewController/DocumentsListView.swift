70f5747c0db872a9070e8315e287762bcdc54566
Rita Lisboa <cassialisboar@gmail.com>
2022-04-03 23:01:21 -0300

    commit

--- RetificaTRANS/View/HomeViewController/DocumentsListView.swift
+++ RetificaTRANS/View/HomeViewController/DocumentsListView.swift
@@ -47,7 +47,7 @@ final class DocumentsListView: UIView {
     private lazy var wellcomeMessage: UILabel = {
         let wellcomeMessage = UILabel()
         wellcomeMessage.textColor = .black
-        wellcomeMessage.font = UIFont.systemFont(ofSize: 35, weight: .bold)
+        wellcomeMessage.font = UIFont.systemFont(ofSize: 35)
         wellcomeMessage.text = "Bem vinde ao [appName]"
         wellcomeMessage.numberOfLines = 2
         wellcomeMessage.lineBreakMode = .byWordWrapping
@@ -75,6 +75,62 @@ final class DocumentsListView: UIView {
         startJourneyContentView.topAnchor.constraint(equalTo: wellcomeMessage.bottomAnchor, constant: 29).isActive = true
         startJourneyContentView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 28).isActive = true
         startJourneyContentView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -28).isActive = true
+        startJourneyContentView.heightAnchor.constraint(equalToConstant: 137).isActive = true
+    }
+    
+    // MARK: - startJourney
+    
+    private lazy var startJourney: UILabel = {
+       let startJourney = UILabel()
+        startJourney.textColor = .white
+        startJourney.font = UIFont.systemFont(ofSize: 20)
+        startJourney.text = "Iniciar minha jornada"
+        startJourney.translatesAutoresizingMaskIntoConstraints = false
+        return startJourney
+    }()
+    
+    private func constraintsStartJourgey() {
+        startJourney.topAnchor.constraint(equalTo: startJourneyContentView.topAnchor, constant: 40).isActive = true
+        startJourney.leadingAnchor.constraint(equalTo: startJourneyContentView.leadingAnchor, constant: 36).isActive = true
+        startJourney.bottomAnchor.constraint(equalTo: startJourneyContentView.bottomAnchor, constant: -74).isActive = true
+
+    }
+
+    // MARK: - startChevron
+    
+    private lazy var startJourneyChevron: UIImageView = {
+       let startJourneyChevron = UIImageView()
+        startJourneyChevron.tintColor = .white
+        startJourneyChevron.image = UIImage(systemName: "chevron.right")
+        startJourneyChevron.translatesAutoresizingMaskIntoConstraints = false
+        return startJourneyChevron
+    }()
+    
+    private func constraintsstartJourneyChevron() {
+        startJourneyChevron.topAnchor.constraint(equalTo: startJourneyContentView.topAnchor, constant: 44).isActive = true
+        startJourneyChevron.leadingAnchor.constraint(equalTo: startJourney.trailingAnchor, constant: 15).isActive = true
+        startJourneyChevron.bottomAnchor.constraint(equalTo: startJourneyContentView.bottomAnchor, constant: -76.85).isActive = true
+
+    }
+    
+    // MARK: - startDescription
+    
+    private lazy var startDescription: UILabel = {
+       let startDescription = UILabel()
+        startDescription.textColor = .white
+        startDescription.font = UIFont.systemFont(ofSize: 13)
+        startDescription.text = "Acompanhe o seu progresso na solicitação de sua retificação de documentos"
+        startDescription.numberOfLines = 2
+        startDescription.lineBreakMode = .byWordWrapping
+        startDescription.translatesAutoresizingMaskIntoConstraints = false
+        return startDescription
+    }()
+    
+    private func constraintsStartDescription() {
+        startDescription.topAnchor.constraint(equalTo: startJourneyContentView.topAnchor, constant: 71).isActive = true
+        startDescription.leadingAnchor.constraint(equalTo: startJourneyContentView.leadingAnchor, constant: 36).isActive = true
+//        startDescription.bottomAnchor.constraint(equalTo: startJourneyContentView.bottomAnchor, constant: -36).isActive = true
+        startDescription.trailingAnchor.constraint(equalTo: startJourneyContentView.trailingAnchor, constant: -50).isActive = true
     }
     
     // MARK: - inits
@@ -86,6 +142,9 @@ final class DocumentsListView: UIView {
         constraintsGreetingUser()
         constraintWellcomeMessage()
         constraintsStartJourneyContentView()
+        constraintsStartJourgey()
+        constraintsstartJourneyChevron()
+        constraintsStartDescription()
     }
     
     required init?(coder: NSCoder) {
@@ -97,5 +156,8 @@ final class DocumentsListView: UIView {
         addSubview(greetingUser)
         addSubview(wellcomeMessage)
         addSubview(startJourneyContentView)
+        addSubview(startJourney)
+        addSubview(startJourneyChevron)
+        addSubview(startDescription)
     }
 }
