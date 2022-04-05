93353d247256ca98b0f86fc833fc80974bc2d985
Rita Lisboa <cassialisboar@gmail.com>
2022-04-05 17:51:11 -0300

    commit

--- RetificaTRANS/Delegates/SceneDelegate.swift
+++ RetificaTRANS/Delegates/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         window?.makeKeyAndVisible()
-        let nav = UINavigationController(rootViewController: TabBarViewController())
+        let nav = UINavigationController(rootViewController: DocumentsListPageViewController())
         
         window?.rootViewController = nav
     }
